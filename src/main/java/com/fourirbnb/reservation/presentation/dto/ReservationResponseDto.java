83fc4be472ec843e98dd478efb9aa539e04e0b38
Jeongwook Woo <dnwjddnr115@naver.com>
2025-04-24 15:14:24 +0900

    feature: 예약 서비스 캐싱 적용

--- src/main/java/com/fourirbnb/reservation/presentation/dto/ReservationResponseDto.java
+++ src/main/java/com/fourirbnb/reservation/presentation/dto/ReservationResponseDto.java
@@ -1,10 +1,17 @@
 package com.fourirbnb.reservation.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ReservationResponseDto.class, name = "ReservationResponseDTO")
+})
 public record ReservationResponseDto(UUID id, Long userId, UUID lodeId, Long price,
                                      LocalDateTime checkInDate, LocalDateTime checkOutDate,
-                                     String reservationStatus) {
+                                     String reservationStatus) implements Serializable {
 
 }
